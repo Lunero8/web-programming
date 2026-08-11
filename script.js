a3b4c02cedfcedd7a5607c355920c8e3f7df0a17
Sayem <sayemsoftd@gmail.com>
2026-08-11 14:32:21 +0600

    js

--- script.js
+++ script.js
@@ -4,13 +4,17 @@
     ===================================
 
     alert() → shows a popup window with a message
-    alert("Hello!");
+        alert("Hello!");
 
     typeof() → returns the data type of a value (as a string)
-    typeof(123);     // "number"
-    typeof("123");   // "string"
-    typeof(true);    // "boolean"
-    typeof(undefined); // "undefined"  
+        typeof(123);     // "number"
+        typeof("123");   // "string"
+        typeof(true);    // "boolean"
+        typeof(undefined); // "undefined"  
+
+    prompt() → get inputs
+        prompt("Whats your name");
 */
 
-
+    // input
+    prompt("What is your name") ;
